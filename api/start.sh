c9e8b6e4a6e30efdaab254c864825a2b0611bc1e
Théo DARVOUX <theo.darvoux@telecom-sudparis.eu>
2026-05-14 18:56:02 +0200

    chore: optimize Docker image size by pruning unnecessary files and removing uv dependency from runtime

--- api/start.sh
+++ api/start.sh
@@ -4,10 +4,10 @@ set -e
 
 # Wait for postgres to be ready
 echo "Waiting for database..."
-uv run python wait_for_db.py
+python wait_for_db.py
 
 echo "Running migrations..."
-uv run alembic upgrade head
+alembic upgrade head
 
 echo "Starting application..."
 exec "$@"
